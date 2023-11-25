20135a22bb7f9068829f0b8a7998e4f32ed03f99
hyeon9782 <hyeon9782@gmail.com>
2023-11-25 23:47:09 +0900

    feat: Accordion 컴포넌트 구현

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,26 @@
-const Accordion = () => {
-  return <div>Accordion</div>;
+import { ReactNode, useState } from "react";
+
+type AccordionProps = {
+  title: string;
+  children: ReactNode;
+};
+const Accordion = ({ title, children }: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <div
+        onClick={toggleAccordion}
+        style={{ cursor: "pointer" }}
+        className="bg-slate-400 rounded-sm px-3 py-2"
+      >
+        <h2>{title}</h2>
+      </div>
+      {isOpen && <div>{children}</div>}
+    </div>
+  );
 };
 
 export default Accordion;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,18 @@
+import Accordion from "../components/Accordion";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const list = ["제목 1입니다.", "제목 2입니다.", "제목 3입니다."];
+  return (
+    <main className="flex justify-center items-center h-full">
+      <div className="">
+        {list.map((item, index) => (
+          <Accordion title={item} key={index}>
+            <div>{item} 내용이다</div>
+          </Accordion>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
