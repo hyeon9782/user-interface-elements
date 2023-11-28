6ad1cc2438d1eef4e030e4a5d6fdea41d980808a
hyeon9782 <hyeon9782@gmail.com>
2023-11-28 22:27:56 +0900

    feat: SelectBox 컴포넌트 구현

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -1,5 +1,44 @@
-const SelectBox = () => {
-  return <div>SelectBox</div>;
+import { MouseEvent, useState } from "react";
+type Props = {
+  options: [];
+};
+const SelectBox = ({ options }: Props) => {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const select = (e: MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLDivElement;
+
+    setSelectedItem(target.textContent);
+    toggleOpen();
+  };
+
+  return (
+    <section>
+      <button>{selectedItem ?? "open me!"}</button>
+      {open && (
+        <section>
+          {options.map((option, idx) => {
+            return (
+              <div
+                key={idx}
+                onClick={select}
+                className={`optionItem ${
+                  selectedItem === option && "selected"
+                }`}
+              >
+                {option}
+              </div>
+            );
+          })}
+        </section>
+      )}
+    </section>
+  );
 };
 
 export default SelectBox;
