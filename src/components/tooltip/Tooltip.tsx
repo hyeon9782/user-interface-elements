c79df0ce1e779bb5ae079f3b277dc07f68fbd2be
hyeon9782 <hyeon9782@gmail.com>
2023-12-04 14:57:56 +0900

    feat: Tooltip 컴포넌트 구현

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,8 +1,41 @@
+import { ReactNode, useEffect, useState } from "react";
+
 type TooltipProps = {
-  title: string;
+  children: ReactNode;
+  text: string;
 };
-const Tooltip = ({ title }: TooltipProps) => {
-  return <div className="">{title}</div>;
+const Tooltip = ({ children, text }: TooltipProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    if (isHovered) {
+      setIsVisible(true);
+    } else {
+      timeoutId = setTimeout(() => setIsVisible(false), 3000);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isHovered]);
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="relative inline-block"
+    >
+      {children}
+
+      {isVisible && (
+        <div className="absolute top-full left-1/2 translate-x-[-50%] bg-black opacity-60 text-white p-[0.5rem] rounded-md z-10 transition-opacity duration-300">
+          {text}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Tooltip;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
+import Tooltip from "../components/tooltip/Tooltip";
 import useToast from "../hooks/useToast";
 
 const HomePage = () => {
   const { openToast } = useToast();
   return (
     <main className="h-[calc(100%-44px)] ">
-      <button onClick={openToast}>Toast Button</button>
+      <Tooltip text="Modal입니다!">
+        <button onClick={openToast}>Toast Button</button>
+      </Tooltip>
     </main>
   );
 };
