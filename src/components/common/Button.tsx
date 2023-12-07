ce5775df928770aeb049a8e03ada68381346c4ef
hyeon9782 <hyeon9782@gmail.com>
2023-12-07 21:22:12 +0900

    feat: Button 컴포넌트 props에 따른 스타일 분기처리

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,14 +1,72 @@
 import { ReactNode } from "react";
 
+type ButtonVariant = "solid" | "outline";
+
+type ButtonSize = "sm" | "md" | "lg";
+
 type ButtonProps = {
-  color: string;
-  variant: string;
-  size: string;
+  color?: string;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  onClick?: () => void;
   children: ReactNode;
 };
 
-const Button = ({ color, variant, size, children }: ButtonProps) => {
-  return <div>{children}</div>;
+const Button = ({
+  color = "black",
+  variant = "solid",
+  size = "sm",
+  children,
+  ...props
+}: ButtonProps) => {
+  const buttonClass = selectedStyle({ color, variant, size });
+  return (
+    <button {...props} className={"rounded-md font-bold " + buttonClass}>
+      {children}
+    </button>
+  );
 };
+
+function selectedStyle({
+  color,
+  variant,
+  size,
+}: Omit<ButtonProps, "children">): string {
+  let classStr: string = "";
+
+  switch (color) {
+    case "red":
+      classStr += "border-red-400 bg-red-400 text-red ";
+      break;
+    case "blue":
+      classStr += "border-blue-400 bg-blue-400 text-blue ";
+      break;
+  }
+
+  switch (variant) {
+    case "solid":
+      classStr += `bg-${color} border-none text-white `;
+      break;
+    case "outline":
+      classStr += `bg-white border-2 `;
+      break;
+  }
+
+  switch (size) {
+    case "sm":
+      classStr += "px-[20px] py-[10px]";
+      break;
+    case "md":
+      classStr += "px-[30px] py-[15px]";
+      break;
+    case "lg":
+      classStr += "px-[40px] py-[20px]";
+      break;
+  }
+
+  console.log(classStr);
+
+  return classStr;
+}
 
 export default Button;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,30 @@ import { useRef, useState } from "react";
 import Dialog from "../components/dialog/Dialog";
 import Tooltip from "../components/tooltip/Tooltip";
 import useToast from "../hooks/useToast";
+import Button from "../components/common/Button";
 
 const HomePage = () => {
   const { openToast } = useToast();
 
   return (
     <main className="h-[calc(100%-44px)] ">
-      <Tooltip text="Toast입니다!">
+      <Button color="red" size="sm" onClick={() => alert("클릭")}>
+        솔리드 버튼 s
+      </Button>
+      <Button color="red" size="md">
+        솔리드 버튼 m
+      </Button>
+      <Button color="red" size="lg">
+        솔리드 버튼 l
+      </Button>
+      <Button color="red" size="lg" variant="outline">
+        아웃 라인 버튼 l
+      </Button>
+      {/* <Tooltip text="Toast입니다!">
         <button onClick={openToast}>Toast Button</button>
       </Tooltip>
 
-      <Dialog />
+      <Dialog /> */}
     </main>
   );
 };
